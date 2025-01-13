bf722dcdb054c6b91f298e6073a067de3e898b36
incremental capital <incrementalcapital@gmail.com>
2025-01-13 16:50:50 +0900

    This file:
    Sets up React 18's createRoot API
    Wraps the app with StrictMode for development checks
    Provides react-helmet for managing document head
    Sets up routing with BrowserRouter
    Imports global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './styles/globals.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>,
 )
